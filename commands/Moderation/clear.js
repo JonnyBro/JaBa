fb8ba8372702b43d926c03e3a8d6a575917ef415
JonnyBro <niktom.nikto@yandex.ru>
2021-12-18 20:57:47 +0500

    откат

--- commands/Moderation/clear.js
+++ commands/Moderation/clear.js
@@ -65,14 +65,3 @@ class Clear extends Command {
 };
 
 module.exports = Clear;
-
-function getNoun(number, one, two, five) {
-	let n = Math.abs(number);
-	n %= 100;
-	if (n >= 5 && n <= 20) return five;
-	n %= 10;
-	if (n === 1) return one;
-	if (n >= 2 && n <= 4) return two;
-
-	return five;
-};
