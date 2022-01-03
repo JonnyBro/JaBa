d10140c9fabc5cbe0b08de7ed04c96b391a9b0ef
JonnyBro <niktom.nikto@yandex.ru>
2022-01-03 14:35:55 +0500

    Правильные склонения для stats и статуса

--- events/ready.js
+++ events/ready.js
@@ -44,7 +44,7 @@ module.exports = class {
 		let servers = client.guilds.cache.filter(guild => guild.id != "568120814776614924" && guild.id != "892727526911258654").size;
 		const version = require("../package.json").version;
 		const status = [
-			{ name: `${servers} ${client.getNoun(servers, client.translate("misc:NOUNS:SERVERS:1"), client.translate("misc:NOUNS:SERVERS:2"), client.translate("misc:NOUNS:SERVERS:5"))}`, type: "WATCHING" },
+			{ name: `${servers} ${client.getNoun(servers, client.translate("misc:NOUNS:SERVER:1"), client.translate("misc:NOUNS:SERVER:2"), client.translate("misc:NOUNS:SERVER:5"))}`, type: "WATCHING" },
 			{ name: "help", type: "WATCHING" }
 		];
 
