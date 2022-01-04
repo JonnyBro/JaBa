2b2f2829ca749f4d421400a246f30a1ecaced92c
JonnyBro <niktom.nikto@yandex.ru>
2022-01-05 00:58:48 +0500

    Смена мест

--- commands/General/userinfo.js
+++ commands/General/userinfo.js
@@ -67,11 +67,11 @@ class Userinfo extends Command {
 			});
 
 		if (displayPresence) {
-			if (!member.presence.activities[0].name === "Custom Status") {
-				embed.addField(this.client.customEmojis.games + " " + message.translate("common:GAME"), (member.presence.activities[0] ? `${member.presence.activities[0].name}\n${member.presence.activities[0].details}\n${member.presence.activities[0].state}` : message.translate("general/userinfo:NO_GAME")), true)
+			if (member.presence.activities[0].name === "Custom Status") {
+				embed.addField(this.client.customEmojis.games + " " + message.translate("common:GAME"), (member.presence.activities[0] ? `Пользовательский статус\n${member.presence.activities[0].state}` : message.translate("general/userinfo:NO_GAME")), true)
 				embed.addField(this.client.customEmojis.status[member.presence.status] + " " + message.translate("common:STATUS"), message.translate("common:STATUS_" + (member.presence.status.toUpperCase())), true);
 			} else {
-				embed.addField(this.client.customEmojis.games + " " + message.translate("common:GAME"), (member.presence.activities[0] ? `Пользовательский статус\n${member.presence.activities[0].state}` : message.translate("general/userinfo:NO_GAME")), true)
+				embed.addField(this.client.customEmojis.games + " " + message.translate("common:GAME"), (member.presence.activities[0] ? `${member.presence.activities[0].name}\n${member.presence.activities[0].details}\n${member.presence.activities[0].state}` : message.translate("general/userinfo:NO_GAME")), true)
 				embed.addField(this.client.customEmojis.status[member.presence.status] + " " + message.translate("common:STATUS"), message.translate("common:STATUS_" + (member.presence.status.toUpperCase())), true);
 			};
 		};
