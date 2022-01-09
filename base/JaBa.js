d48719612ac97b61c73fa35dbf3f80ab9cfc3f2f
JonnyBro <niktom.nikto@yandex.ru>
2022-01-10 00:40:25 +0500

    smol change

--- base/JaBa.js
+++ base/JaBa.js
@@ -71,7 +71,7 @@ class JaBa extends Client {
 				} else {
 					setTimeout(() => {
 						if (m.deletable) m.delete();
-					}, 600 * 1000);
+					}, 60 * 10 * 1000);
 				}
 			})
 			.on("addSong", (queue, song) => queue.textChannel.send({ content: this.translate("music/play:ADDED_QUEUE", { songName: song.name }) }))
