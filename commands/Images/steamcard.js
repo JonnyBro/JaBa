31205292d751949fbae934f1617f9174fe21c656
JonnyBro <niktom.nikto@yandex.ru>
2022-01-24 18:36:48 +0500

    v3.3.3 Новая генерация steamcard

---
+++ commands/Images/steamcard.js
@@ -0,0 +1,45 @@
+const Command = require("../../base/Command");
+
+class SteamCard extends Command {
+	constructor(client) {
+		super(client, {
+			name: "steamcard",
+			dirname: __dirname,
+			enabled: true,
+			guildOnly: false,
+			aliases: ["sc"],
+			memberPermissions: [],
+			botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "ATTACH_FILES"],
+			nsfw: false,
+			ownerOnly: false,
+			cooldown: 3000
+		});
+	}
+
+	async run(message, args) {
+		const text = args.join(" ");
+		if (!text) return message.error("images/qrcode:MISSING_TEXT");
+
+		const user = await this.client.resolveUser(args[0]) || message.author;
+		const m = await message.sendT("misc:PLEASE_WAIT", null, {
+			prefixEmoji: "loading"
+		});
+		const buffer = await this.client.AmeAPI.generate("steamcard", {
+			text: text,
+			url: user.displayAvatarURL({
+				format: "png",
+				size: 512
+			})
+		});
+		m.delete();
+
+		message.channel.send({
+			files: [{
+				name: "steamcard.png",
+				attachment: buffer
+			}]
+		});
+	}
+}
+
+module.exports = SteamCard;
