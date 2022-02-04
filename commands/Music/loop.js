969336e8b1fb0052a0d7b6f065fe3f64462e16e6
JonnyBro <niktom.nikto@yandex.ru>
2022-02-04 17:02:07 +0500

    Мелкие правки

--- commands/Music/loop.js
+++ commands/Music/loop.js
@@ -18,12 +18,12 @@ class Loop extends Command {
 
 	async run(message, args) {
 		const voice = message.member.voice.channel;
-		const type = args[0];
 		const queue = this.client.player.getQueue(message);
 
 		if (!voice) return message.error("music/play:NO_VOICE_CHANNEL");
 		if (!queue) return message.error("music/play:NOT_PLAYING");
 
+		const type = args[0];
 		let mode = null;
 		if (type === "queue") {
 			mode = this.client.player.setRepeatMode(message, 2);
