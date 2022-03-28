4f3c0a20c70f42fb4a0805230ed9f5253ddfb7c7
JonnyBro <niktom.nikto@yandex.ru>
2022-03-28 23:34:43 +0500

    Тест

--- dashboard/routes/docs.js
+++ dashboard/routes/docs.js
@@ -5,7 +5,8 @@ router.get("/", function (req, res) {
 	res.render("docs", {
 		user: req.userInfos,
 		translate: req.translate,
-		currentURL: `${req.client.config.dashboard.baseURL}${req.originalUrl}`
+		currentURL: `${req.client.config.dashboard.baseURL}${req.originalUrl}`,
+		site: req.client.config.dashboard.baseURL
 	});
 });
 
