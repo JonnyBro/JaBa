f52ddc6e2562ed5f0a9175f1b62d744ee53c785f
JonnyBro <niktom.nikto@yandex.ru>
2022-10-03 20:27:51 +0500

    Да блять

--- helpers/extenders.js
+++ helpers/extenders.js
@@ -38,8 +38,8 @@ Message.prototype.replyT = function (key, args, options = {}) {
 	let string = this.translate(key, args, this.guild ? this.guild.data.language : "ru-RU");
 	if (options.prefixEmoji) string = `${this.client.customEmojis[options.prefixEmoji]} | ${string}`;
 
-	if (options.edit) return this.edit({ content: string, allowedMentions: { repliedUser: options.mention || true } });
-	else return this.reply({ content: string, allowedMentions: { repliedUser: options.mention || true } });
+	if (options.edit) return this.edit({ content: string, allowedMentions: { repliedUser: options.mention ? true : false } });
+	else return this.reply({ content: string, allowedMentions: { repliedUser: options.mention ? true : false } });
 };
 
 Message.prototype.error = function (key, args, options = {}) {
