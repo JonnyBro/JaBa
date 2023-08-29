0d5ddc5fcaed608e5de0d7b6faa30312a5077b5e
Jonny_Bro (Nikita) <48434875+JonnyBro@users.noreply.github.com>
2023-08-29 12:18:36 +0500

    add missing

--- commands/Administration/config.js
+++ commands/Administration/config.js
@@ -156,7 +156,7 @@ class Config extends BaseCommand {
 					{
 						name: interaction.translate("administration/config:SPECIAL_CHANNELS"),
 						value:
-							`${interaction.translate("administration/config:BIRTHDAYS")}: ${guildData.plugins.birthdays ? `<#${guildData.plugins.birthdays}>` : `*${interaction.translate("common:NOT_DEFINED")}*`}` +
+							`${interaction.translate("administration/config:BIRTHDAYS")}: ${guildData.plugins.birthdays ? `<#${guildData.plugins.birthdays}>` : `*${interaction.translate("common:NOT_DEFINED")}*`}\n` +
 							`${interaction.translate("administration/config:MODLOGS")}: ${guildData.plugins.modlogs ? `<#${guildData.plugins.modlogs}>` : `*${interaction.translate("common:NOT_DEFINED")}*`}\n` +
 							`${interaction.translate("administration/config:REPORTS")}: ${guildData.plugins.reports ? `<#${guildData.plugins.reports}>` : `*${interaction.translate("common:NOT_DEFINED")}*`}\n` +
 							`${interaction.translate("administration/config:SUGGESTIONS")}: ${guildData.plugins.suggestions ? `<#${guildData.plugins.suggestions}>` : `*${interaction.translate("common:NOT_DEFINED")}*`}\n` +
