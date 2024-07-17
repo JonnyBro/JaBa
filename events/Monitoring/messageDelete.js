b2070ba6cbf71a66eb0d7bc6ff284ce232a797b8
Jonny_Bro (Nikita) <niktom.nikto@ya.ru>
2024-07-17 20:30:01 +0500

    add deleted by

--- events/Monitoring/messageDelete.js
+++ events/Monitoring/messageDelete.js
@@ -20,13 +20,22 @@ class messageDelete extends BaseEvent {
 		const guildData = message.data.guild;
 
 		if (guildData.plugins?.monitoring?.messageDelete) {
+			await client.wait(1000);
+
+			const logs = await message.guild.fetchAuditLogs({
+				limit: 5,
+				type: "MESSAGE_DELETE",
+			});
+
+			const deletedMessage = logs.entries.find(e => e.targetId === message.author.id && e.extra.channel.id && Date.now() - e.createdTimestamp < 20000);
+
 			const embed = client.embed({
 				author: {
 					name: message.author.getUsername(),
 					iconURL: message.author.displayAvatarURL(),
 				},
 				title: message.translate("misc:MONITORING:DELETE:TITLE", { user: message.author.getUsername() }),
-				description: message.translate("misc:MONITORING:DELETE:DESCRIPTION", { content: message.content, channel: message.channel.toString(), time: `<t:${Math.floor(message.createdTimestamp / 1000)}:f>` }),
+				description: message.translate("misc:MONITORING:DELETE:DESCRIPTION", { deletedBy: deletedMessage.executor ? deletedMessage.executor.toString() : "Unknown", content: message.content, channel: message.channel.toString(), time: `<t:${Math.floor(message.createdTimestamp / 1000)}:f>` }),
 			});
 
 			message.guild.channels.cache.get(guildData.plugins.monitoring.messageDelete).send({
