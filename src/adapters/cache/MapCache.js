ac461a104d68aa8d9cca996cb8d625cf8bf507a6
Slincnik <pattyshow21@mail.ru>
2024-12-16 17:41:11 +0300

    feat: added cache adapter

---
+++ src/adapters/cache/MapCache.js
@@ -0,0 +1,24 @@
+import ICacheAdapter from "./ICacheAdapter";
+
+export default class MapCache extends ICacheAdapter {
+	constructor() {
+		super();
+		this.store = new Map();
+	}
+
+	get(key) {
+		return this.store.get(key);
+	}
+
+	set(key, value) {
+		this.store.set(key, value);
+	}
+
+	clear() {
+		this.store.clear();
+	}
+
+	delete(key) {
+		this.store.delete(key);
+	}
+}
