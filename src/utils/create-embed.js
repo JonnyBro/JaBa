e1e8963306a39d40ae86f4de24d3240872d225de
Slincnik <pattyshow21@mail.ru>
2025-01-11 13:54:21 +0300

    fix(embed): fixed color error and inability to add to messages embed

--- src/utils/create-embed.js
+++ src/utils/create-embed.js
@@ -7,9 +7,8 @@ import useClient from "../utils/use-client.js";
  */
 export const createEmbed = data => {
 	const client = useClient();
-	new EmbedBuilder({
+	return new EmbedBuilder({
 		footer: typeof data.footer === "object" ? data.footer : data.footer ? { text: data.footer } : client.configService.get("embed.footer"),
-		color: data.color ?? client.configService.get("embed.color"),
 		...data,
-	});
+	}).setColor(data.color ?? client.configService.get("embed.color"));
 };
