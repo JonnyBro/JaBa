b54a793002c95cae9d1eefb796b201c6759452dc
Slincnik <pattyshow21@mail.ru>
2025-03-08 15:44:06 +0300

    refactor: generateFields func

--- src/commands/Administration/config.ts
+++ src/commands/Administration/config.ts
@@ -2,7 +2,9 @@ import { getLocalizedDesc, replyError, translateContext } from "@/helpers/extend
 import { CommandData, SlashCommandProps } from "@/types.js";
 import { createEmbed } from "@/utils/create-embed.js";
 import useClient from "@/utils/use-client.js";
-import { ApplicationCommandOptionType, ApplicationIntegrationType, ChannelType, ChatInputCommandInteraction, InteractionContextType, MessageFlags, PermissionsBitField } from "discord.js";
+import { ApplicationCommandOptionType, ApplicationIntegrationType, Channel, ChannelType, ChatInputCommandInteraction, InteractionContextType, MessageFlags, PermissionsBitField } from "discord.js";
+import GuildModel from "@/models/GuildModel.js";
+import { generateFields } from "@/utils/config-fields.js";
 
 const client = useClient();
 
@@ -74,9 +76,6 @@ export const run = async ({ interaction }: SlashCommandProps) => {
 				name: interaction.guild.name,
 				iconURL: interaction.guild.iconURL() || "",
 			},
-			// TODO: тс даёт ошибку но работает ( я ещё учу тс ;( )
-			// Тип "({ name: string; value: string; inline: boolean; } | undefined)[]" не может быть назначен для типа "readonly APIEmbedField[]".
-			// @ts-ignore Type not assignable (for now)
 			fields,
 		});
 
@@ -85,84 +84,19 @@ export const run = async ({ interaction }: SlashCommandProps) => {
 
 	const parameter = interaction.options.getString("parameter", true);
 	const state = interaction.options.getBoolean("state", true);
-	const channel = interaction.options.getChannel("channel");
+	const channel = interaction.options.getChannel("channel") as Channel;
 
 	await changeSetting(interaction, guildData, parameter, state, channel);
 };
 
-async function generateFields(interaction: ChatInputCommandInteraction, guildData: any) {
-	const fieldsConfig = [
-		{
-			nameKey: "administration/config:WELCOME_TITLE",
-			valueKey: "administration/config:WELCOME_CONTENT",
-			plugin: guildData.plugins.welcome,
-		},
-		{
-			nameKey: "administration/config:GOODBYE_TITLE",
-			valueKey: "administration/config:GOODBYE_CONTENT",
-			plugin: guildData.plugins.goodbye,
-		},
-		{
-			nameKey: "administration/config:MONITORING_CHANNELS",
-			values: [
-				{ key: "administration/config:MESSAGEUPDATE", value: guildData.plugins?.monitoring?.messageUpdate },
-				{ key: "administration/config:MESSAGEDELETE", value: guildData.plugins?.monitoring?.messageDelete },
-			],
-		},
-		{
-			nameKey: "administration/config:SPECIAL_CHANNELS",
-			values: [
-				{ key: "administration/config:BIRTHDAYS", value: guildData.plugins?.birthdays },
-				{ key: "administration/config:MODLOGS", value: guildData.plugins?.modlogs },
-				{ key: "administration/config:REPORTS", value: guildData.plugins?.reports },
-				{ key: "administration/config:SUGGESTIONS", value: guildData.plugins?.suggestions },
-				{ key: "administration/config:TICKETSCATEGORY", value: guildData.plugins?.tickets?.ticketsCategory },
-				{ key: "administration/config:TICKETLOGS", value: guildData.plugins?.tickets?.ticketLogs },
-				{ key: "administration/config:TRANSCRIPTIONLOGS", value: guildData.plugins?.tickets?.transcriptionLogs },
-			],
-		},
-	];
-
-	const fields = await Promise.all(
-		fieldsConfig.map(async field => {
-			const name = await translateContext(interaction, field.nameKey);
-
-			if (field.plugin) {
-				const value = field.plugin.enabled
-					? await translateContext(interaction, field.valueKey!, {
-						channel: `<#${field.plugin.channel}>`,
-					})
-					: await translateContext(interaction, "common:DISABLED");
-
-				return { name, value, inline: true };
-			}
-
-			if (field.values) {
-				const value = await Promise.all(
-					field.values.map(async ({ key, value }) => {
-						const translatedKey = await translateContext(interaction, key);
-						const translatedValue = value ? `<#${value}>` : `*${await translateContext(interaction, "common:NOT_DEFINED")}*`;
-
-						return `${translatedKey}: ${translatedValue}`;
-					}),
-				);
-
-				return { name, value: value.join("\n"), inline: false };
-			}
-		}),
-	);
-
-	return fields;
-}
-
-async function saveSettings(guildData: any, parameter: string, value: any) { // TODO: Proper type for `any`
+async function saveSettings(guildData: InstanceType<typeof GuildModel>, parameter: string, value: unknown) {
 	guildData.plugins[parameter] = value;
 	guildData.markModified(`plugins.${parameter}`);
 
 	await guildData.save();
 }
 
-async function generateReply(interaction: ChatInputCommandInteraction, guildData: any, parameter: string, state: boolean, channel?: any) {
+async function generateReply(interaction: ChatInputCommandInteraction, guildData: InstanceType<typeof GuildModel>, parameter: string, state: boolean, channel?: Channel | null) {
 	const translatedParam = await translateContext(interaction, `administration/config:${parameter.toUpperCase()}`);
 	const enabledText = await translateContext(interaction, "common:ENABLED");
 	const disabledText = await translateContext(interaction, "common:DISABLED");
@@ -172,7 +106,7 @@ async function generateReply(interaction: ChatInputCommandInteraction, guildData
 	return `${translatedParam}: ${state ? `**${enabledText}** (<#${guildData.plugins[parameter]}>)` : `**${disabledText}**`}`;
 }
 
-async function changeSetting(interaction: ChatInputCommandInteraction, guildData: any, parameter: string, state: boolean, channel?: any) {
+async function changeSetting(interaction: ChatInputCommandInteraction, guildData: any, parameter: string, state: boolean, channel?: Channel | null) {
 	const parameterSplitted = parameter.split(".");
 	const isNested = parameterSplitted.length === 2;
 
