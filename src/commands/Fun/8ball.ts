bdf52b0fa174e6b24c94869c3999e2e0db568d81
Jonny_Bro <48434875+JonnyBro@users.noreply.github.com>
2025-04-24 22:27:30 +0500

    refactor: rewrite `8ball`

---
+++ src/commands/Fun/8ball.ts
@@ -0,0 +1,48 @@
+import { getLocalizedDesc, translateContext } from "@/helpers/extenders.js";
+import { randomNum } from "@/helpers/functions.js";
+import { CommandData, SlashCommandProps } from "@/types.js";
+import { createEmbed } from "@/utils/create-embed.js";
+import { ApplicationCommandOptionType, ApplicationIntegrationType, InteractionContextType, MessageFlags } from "discord.js";
+
+export const data: CommandData = {
+	name: "8ball",
+	...getLocalizedDesc("fun/8ball:DESCRIPTION"),
+	// eslint-disable-next-line camelcase
+	integration_types: [ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall],
+	contexts: [InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel],
+	options: [
+		{
+			name: "question",
+			type: ApplicationCommandOptionType.String,
+			...getLocalizedDesc("fun/8ball:QUESTION"),
+			required: true,
+		},
+		{
+			name: "ephemeral",
+			type: ApplicationCommandOptionType.Boolean,
+			...getLocalizedDesc("misc:EPHEMERAL_RESPONSE"),
+		},
+	],
+};
+
+export const run = async ({ interaction }: SlashCommandProps) => {
+	await interaction.deferReply({ flags: interaction.options.getBoolean("ephemeral") ? MessageFlags.Ephemeral : undefined });
+
+	const question = interaction.options.getString("question", true);
+	const embed = createEmbed({
+		fields: [
+			{
+				name: await translateContext(interaction, "fun/8ball:QUESTION"),
+				value: question,
+			},
+			{
+				name: await translateContext(interaction, "fun/8ball:ANSWER"),
+				value: await translateContext(interaction, `fun/8ball:RESPONSE_${randomNum(1, 20)}`),
+			},
+		],
+	});
+
+	setTimeout(() => {
+		interaction.editReply({ embeds: [embed] });
+	}, 5000);
+};
