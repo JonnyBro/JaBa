4ddde16f9ff39360f5c6f0acabe7622bc36d064d
Jonny_Bro <48434875+JonnyBro@users.noreply.github.com>
2025-04-24 22:28:00 +0500

    refactor: rewrite `lmgtfy`

---
+++ src/commands/Fun/lmgtfy.ts
@@ -0,0 +1,67 @@
+import { getLocalizedDesc } from "@/helpers/extenders.js";
+import { CommandData, SlashCommandProps } from "@/types.js";
+import { createEmbed } from "@/utils/create-embed.js";
+// import useClient from "@/utils/use-client.js";
+import { ApplicationCommandOptionType, ApplicationIntegrationType, InteractionContextType, MessageFlags } from "discord.js";
+
+// const client = useClient();
+
+export const data: CommandData = {
+	name: "lmgtfy",
+	...getLocalizedDesc("fun/lmgtfy:DESCRIPTION"),
+	// eslint-disable-next-line camelcase
+	integration_types: [ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall],
+	contexts: [InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel],
+	options: [
+		{
+			name: "query",
+			type: ApplicationCommandOptionType.String,
+			...getLocalizedDesc("fun/lmgtfy:QUERY"),
+			required: true,
+		},
+		{
+			name: "short",
+			type: ApplicationCommandOptionType.Boolean,
+			...getLocalizedDesc("fun/lmgtfy:SHORT"),
+		},
+		{
+			name: "ephemeral",
+			type: ApplicationCommandOptionType.Boolean,
+			...getLocalizedDesc("misc:EPHEMERAL_RESPONSE"),
+		},
+	],
+};
+
+export const run = async ({ interaction }: SlashCommandProps) => {
+	await interaction.deferReply({ flags: interaction.options.getBoolean("ephemeral") ? MessageFlags.Ephemeral : undefined });
+
+	const query = interaction.options.getString("query", true).replace(/[' '_]/g, "+");
+	const short = interaction.options.getBoolean("short");
+	const url = `https://letmegooglethat.com/?q=${encodeURIComponent(query)}`;
+	const embed = createEmbed({
+		description: url,
+	});
+
+	if (short) {
+		// const res = await fetch("https://i.jonnybro.ru/api/shorten", {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 		Authorization: client.configService.get("apiKeys.zipline"),
+		// 	},
+		// 	body: JSON.stringify({ url: url }),
+		// }).then(res => res.json());
+
+		// interaction.editReply({
+		// 	content: `<${res.url}>`,
+		// });
+
+		interaction.editReply({
+			embeds: [embed],
+		});
+	} else {
+		interaction.editReply({
+			embeds: [embed],
+		});
+	}
+};
