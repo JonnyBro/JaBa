506f38a6408a99e7b1fd4f70976fc140d5e35369
Jonny_Bro <48434875+JonnyBro@users.noreply.github.com>
2025-04-25 18:01:22 +0500

    refactor: createEmbed without arguments

--- src/utils/create-embed.ts
+++ src/utils/create-embed.ts
@@ -1,10 +1,10 @@
 import { EmbedBuilder, EmbedData } from "discord.js";
 import useClient from "./use-client.js";
 
-export const createEmbed = (data: EmbedData) => {
+export const createEmbed = (data?: EmbedData) => {
 	const client = useClient();
 	return new EmbedBuilder({
-		footer: typeof data.footer === "object" ? data.footer : data.footer ? { text: data.footer } : client.configService.get("embed.footer"),
+		footer: typeof data?.footer === "object" ? data.footer : data?.footer ? { text: data.footer } : client.configService.get("embed.footer"),
 		...data,
-	}).setColor(data.color || client.configService.get("embed.color"));
+	}).setColor(data?.color || client.configService.get("embed.color"));
 };
