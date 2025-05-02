d1d9367269edc554d9fa0b5df781ad7643e74adc
Slincnik <pattyshow21@mail.ru>
2025-05-02 14:56:39 +0300

    build: updated eslint and prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default [
 			"@stylistic": stylistic,
 		},
 		rules: {
+			"max-len": ["error", { code: 100 }],
 			"no-unused-vars": "off",
 			"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" }],
 			"arrow-body-style": ["error", "as-needed"],
