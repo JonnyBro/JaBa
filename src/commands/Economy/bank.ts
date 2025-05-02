b8b8186ad5d4dd72ae1663fb37eb61f0ded78cd1
Slincnik <pattyshow21@mail.ru>
2025-05-02 14:03:20 +0300

    refactor(bank): fixed trying async foreach guilds

--- src/commands/Economy/bank.ts
+++ src/commands/Economy/bank.ts
@@ -3,7 +3,7 @@ import { asyncForEach, getNoun } from "@/helpers/functions.js";
 import { CommandData, SlashCommandProps } from "@/types.js";
 import { createEmbed } from "@/utils/create-embed.js";
 import useClient from "@/utils/use-client.js";
-import { ApplicationCommandOptionType, ApplicationIntegrationType, InteractionContextType } from "discord.js";
+import { ApplicationCommandOptionType, ApplicationIntegrationType, CacheType, ChatInputCommandInteraction, InteractionContextType } from "discord.js";
 
 const client = useClient();
 
@@ -38,112 +38,120 @@ export const data: CommandData = {
 	],
 };
 
+async function formatCredits(interaction: ChatInputCommandInteraction<CacheType>, amount: number) {
+	const forms = [await translateContext(interaction, "misc:NOUNS:CREDIT:1"), await translateContext(interaction, "misc:NOUNS:CREDIT:2"), await translateContext(interaction, "misc:NOUNS:CREDIT:5")];
+	const noun = getNoun(amount, forms);
+	return `**${amount}** ${noun}`;
+}
+
 export const run = async ({ interaction }: SlashCommandProps) => {
 	await interaction.deferReply();
 
-	const memberData = await client.getMemberData(interaction.user.id, interaction.guildId!);
+	const memberId = interaction.user.id;
+	const guildId = interaction.guildId!;
+	const memberData = await client.getMemberData(memberId, guildId);
 	const choice = interaction.options.getString("option", true);
+	const creditsChoice = interaction.options.getString("credits");
 
-	const creditsChoise = interaction.options.getString("credits");
-	if (!creditsChoise) return editReplyError(interaction, "misc:OPTION_NAN_ALL");
-
-	const credits = creditsChoise.toLowerCase() === "all" ? memberData.money : Number(creditsChoise);
-
-	if (isNaN(credits) || credits < 1) return editReplyError(interaction, "misc:OPTION_NAN_ALL");
-	if (memberData.money < credits) {
-		return editReplyError(interaction, "economy/bank:NOT_ENOUGH_CREDIT", {
-			money: `**${credits}** ${getNoun(credits, [await translateContext(interaction, "misc:NOUNS:CREDIT:1"), await translateContext(interaction, "misc:NOUNS:CREDIT:2"), await translateContext(interaction, "misc:NOUNS:CREDIT:5")])}`,
-		});
+	if (!creditsChoice) {
+		return editReplyError(interaction, "misc:OPTION_NAN_ALL");
 	}
 
-	const info = {
-		user: await translateContext(interaction, "economy/transactions:BANK"),
-		amount: credits,
-		date: Date.now(),
-		type: "send",
-	};
+	const credits = creditsChoice.toLowerCase() === "all" ? memberData.money : Number(creditsChoice);
+
+	if (isNaN(credits) || credits < 1) {
+		return editReplyError(interaction, "misc:OPTION_NAN_ALL");
+	}
 
 	const embed = createEmbed();
 
-	if (choice === "deposit") {
-		memberData.money -= credits;
-		memberData.bankSold += credits;
+	switch (choice) {
+		case "deposit": {
+			if (memberData.money < credits) {
+				return editReplyError(interaction, "economy/bank:NOT_ENOUGH_CREDIT", {
+					money: await formatCredits(interaction, credits),
+				});
+			}
+			memberData.money -= credits;
+			memberData.bankSold += credits;
+			memberData.transactions.push({
+				user: await translateContext(interaction, "economy/transactions:BANK"),
+				amount: credits,
+				date: Date.now(),
+				type: "send",
+			});
+			await memberData.save();
 
-		memberData.transactions.push(info);
+			embed.setDescription(
+				await translateContext(interaction, "economy/bank:SUCCESS_DEP", {
+					money: await formatCredits(interaction, credits),
+				}),
+			);
+			break;
+		}
 
-		memberData.markModified("money");
-		memberData.markModified("bankSold");
-		memberData.markModified("transactions");
-		await memberData.save();
+		case "withdraw": {
+			if (memberData.bankSold < credits) {
+				return editReplyError(interaction, "economy/bank:NOT_ENOUGH_CREDIT", {
+					money: await formatCredits(interaction, credits),
+				});
+			}
+			memberData.money += credits;
+			memberData.bankSold -= credits;
+			memberData.transactions.push({
+				user: await translateContext(interaction, "economy/transactions:BANK"),
+				amount: credits,
+				date: Date.now(),
+				type: "got",
+			});
+			await memberData.save();
 
-		embed.setDescription(
-			await translateContext(interaction, "economy/bank:SUCCESS_DEP", {
-				money: `**${credits}** ${getNoun(credits, [await translateContext(interaction, "misc:NOUNS:CREDIT:1"), await translateContext(interaction, "misc:NOUNS:CREDIT:2"), await translateContext(interaction, "misc:NOUNS:CREDIT:5")])}`,
-			}),
-		);
+			embed.setDescription(
+				await translateContext(interaction, "economy/bank:SUCCESS_WD", {
+					money: await formatCredits(interaction, credits),
+				}),
+			);
+			break;
+		}
+		case "balance": {
+			const targetUser = interaction.options.getUser("user") || interaction.user;
+			if (targetUser.bot) {
+				return editReplyError(interaction, "economy/money:BOT_USER");
+			}
 
-		interaction.editReply({
-			embeds: [embed],
-		});
-	} else if (choice === "withdraw") {
-		memberData.money += credits;
-		memberData.bankSold -= credits;
+			const targetData = targetUser.id === interaction.user.id ? memberData : await client.getMemberData(targetUser.id, guildId);
 
-		info.type = "got";
+			let globalMoney = 0;
+			const guilds = client.guilds.cache.filter(g => g.members.cache.has(targetUser.id));
+			const guldsArray = Array.from(guilds.values());
+			await asyncForEach(guldsArray, async guild => {
+				const data = await client.getMemberData(targetUser.id, guild.id);
+				globalMoney += data.money + data.bankSold;
+			});
 
-		memberData.transactions.push(info);
-
-		memberData.markModified("money");
-		memberData.markModified("bankSold");
-		memberData.markModified("transactions");
-		await memberData.save();
-
-		embed.setDescription(
-			await translateContext(interaction, "economy/bank:SUCCESS_WD", {
-				money: `**${credits}** ${getNoun(credits, [await translateContext(interaction, "misc:NOUNS:CREDIT:1"), await translateContext(interaction, "misc:NOUNS:CREDIT:2"), await translateContext(interaction, "misc:NOUNS:CREDIT:5")])}`,
-			}),
-		);
-	} else {
-		const user = interaction.options.getUser("user") || interaction.user;
-		if (user.bot) return editReplyError(interaction, "economy/money:BOT_USER");
-
-		const memberDataBank = user.id === interaction.user.id ? memberData : await client.getMemberData(user.id, interaction.guildId!);
-		const guilds = client.guilds.cache.filter(g => g.members.cache.find(m => m.id === user.id));
-
-		let globalMoney = 0;
-		await asyncForEach(guilds, async guild => {
-			const data = await client.getMemberData(user.id, guild.id);
-			globalMoney += data.money + data.bankSold;
-		});
-
-
-		embed
-			// .setAuthor({
-			// 	name: await translateContext(interaction, "economy/money:TITLE", {
-			// 		user: getUsername(user),
-			// 	}),
-			// 	iconURL: user.displayAvatarURL(),
-			// })
-			.setFields([
+			embed.setFields([
 				{
 					name: await translateContext(interaction, "economy/profile:CASH"),
-					value: `**${memberDataBank.money}** ${getNoun(memberDataBank.money, [await translateContext(interaction, "misc:NOUNS:CREDIT:1"), await translateContext(interaction, "misc:NOUNS:CREDIT:2"), await translateContext(interaction, "misc:NOUNS:CREDIT:5")])}`,
+					value: await formatCredits(interaction, targetData.money),
 					inline: true,
 				},
 				{
 					name: await translateContext(interaction, "economy/profile:BANK"),
-					value: `**${memberDataBank.bankSold}** ${getNoun(memberDataBank.bankSold, [await translateContext(interaction, "misc:NOUNS:CREDIT:1"), await translateContext(interaction, "misc:NOUNS:CREDIT:2"), await translateContext(interaction, "misc:NOUNS:CREDIT:5")])}`,
+					value: await formatCredits(interaction, targetData.bankSold),
 					inline: true,
 				},
 				{
 					name: await translateContext(interaction, "economy/profile:GLOBAL"),
-					value: `**${globalMoney}** ${getNoun(globalMoney, [await translateContext(interaction, "misc:NOUNS:CREDIT:1"), await translateContext(interaction, "misc:NOUNS:CREDIT:2"), await translateContext(interaction, "misc:NOUNS:CREDIT:5")])}`,
+					value: await formatCredits(interaction, globalMoney),
 					inline: true,
 				},
 			]);
+			break;
+		}
 
-		interaction.editReply({
-			embeds: [embed],
-		});
+		default:
+			return editReplyError(interaction, "misc:OPTION_NAN_ALL");
 	}
+
+	await interaction.editReply({ embeds: [embed] });
 };
