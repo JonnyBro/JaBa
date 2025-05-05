a4b9790314ebb1f6e5737a7cdb486f60f329a620
Jonny_Bro <48434875+JonnyBro@users.noreply.github.com>
2025-05-05 11:33:49 +0500

    feat: achievements button in `profile` refactor: better `profile` look

--- src/commands/Economy/profile.ts
+++ src/commands/Economy/profile.ts
@@ -1,9 +1,19 @@
-import { asyncForEach, editReplyError, getLocalizedDesc, getXpForNextLevel, translateContext } from "@/helpers/functions.js";
+import {
+	asyncForEach,
+	editReplyError,
+	getLocalizedDesc,
+	getXpForNextLevel,
+	translateContext,
+} from "@/helpers/functions.js";
 import { CommandData, SlashCommandProps } from "@/types.js";
+import { createEmbed } from "@/utils/create-embed.js";
 import useClient from "@/utils/use-client.js";
 import {
+	ActionRowBuilder,
 	ApplicationCommandOptionType,
 	ApplicationIntegrationType,
+	ButtonBuilder,
+	ButtonStyle,
 	ContainerBuilder,
 	InteractionContextType,
 	MessageFlags,
@@ -13,6 +23,8 @@ import {
 } from "discord.js";
 
 const client = useClient();
+
+const ACHIEVEMENTS_BUTTON_ID = "achievements_button";
 
 export const data: CommandData = {
 	name: "profile",
@@ -44,7 +56,9 @@ export const run = async ({ interaction }: SlashCommandProps) => {
 
 	const memberData = await client.getMemberData(user.id, interaction.guildId!);
 	const userData = await client.getUserData(user.id);
-	if (userData.lover && !client.users.cache.has(userData.lover)) await client.users.fetch(userData.lover);
+	if (userData.lover && !client.users.cache.has(userData.lover)) {
+		await client.users.fetch(userData.lover);
+	}
 
 	let globalMoney = 0;
 	const guilds = client.guilds.cache.filter(g => g.members.cache.has(user.id));
@@ -58,30 +72,49 @@ export const run = async ({ interaction }: SlashCommandProps) => {
 	const lover = client.users.cache.get(userData.lover);
 	const notDefined = await translateContext(interaction, "common:NOT_DEFINED");
 	const content = `
-		${await translateContext(interaction, "economy/profile:CREDITS", { credits: memberData.money })}
-		${await translateContext(interaction, "economy/profile:BANK", { credits: memberData.bankSold })}
-		${await translateContext(interaction, "economy/profile:GLOBAL", { credits: globalMoney })}
-		${await translateContext(interaction, "economy/profile:REPUTATION", { rep: userData.rep })}
-		${await translateContext(interaction, "economy/profile:LEVEL", { level: memberData.level })}
-		${await translateContext(interaction, "economy/profile:XP", { xp: memberData.exp, totalXp: getXpForNextLevel(memberData.level) })}
-		${await translateContext(interaction, "economy/profile:BIRTHDATE", { credits: userData.birthdate || notDefined })}
-		${await translateContext(interaction, "economy/profile:LOVER", { user: lover?.toString() || notDefined })}
-		${await translateContext(interaction, "economy/profile:REGISTERED", { date: `<t:${Math.floor(memberData.registeredAt / 1000)}:f>` })}
-	`;
+${await translateContext(interaction, "economy/profile:CREDITS", {
+	credits: memberData.money,
+})}
+${await translateContext(interaction, "economy/profile:BANK", {
+	credits: memberData.bankSold,
+})}
+${await translateContext(interaction, "economy/profile:GLOBAL", { credits: globalMoney })}
+${await translateContext(interaction, "economy/profile:REPUTATION", { rep: userData.rep })}
+${await translateContext(interaction, "economy/profile:LEVEL", { level: memberData.level })}
+${await translateContext(interaction, "economy/profile:XP", {
+	xp: memberData.exp,
+	totalXp: getXpForNextLevel(memberData.level),
+})}
+${await translateContext(interaction, "economy/profile:BIRTHDATE", {
+	birthdate: userData.birthdate ? `<t:${Math.floor(userData.birthdate / 1000)}:f>` : notDefined,
+})}
+${await translateContext(interaction, "economy/profile:LOVER", {
+	user: lover?.toString() || notDefined,
+})}
+${await translateContext(interaction, "economy/profile:REGISTERED", {
+	date: `<t:${Math.floor(memberData.registeredAt / 1000)}:f>`,
+})}
+`;
 
 	const container = new ContainerBuilder()
 		.addSectionComponents(
 			new SectionBuilder()
 				.setThumbnailAccessory(t => t.setURL(user.avatarURL({ size: 2048 }) || ""))
 				.addTextDisplayComponents(
-					new TextDisplayBuilder().setContent(`
-						# ${await translateContext(interaction, "economy/profile:TITLE", { user: user.toString() })}
-						${userData.bio ? `### ${userData.bio}` : ""}`),
-				)
+					new TextDisplayBuilder().setContent(
+						`# ${user.toString()}${userData.bio ? `\n## ${userData.bio}` : ""}\n`,
+					),
+				),
 		)
 		.addSeparatorComponents(s => s.setSpacing(SeparatorSpacingSize.Small).setDivider(true))
-		.addTextDisplayComponents(
-			new TextDisplayBuilder().setContent(content)
+		.addTextDisplayComponents(t => t.setContent(content))
+		.addActionRowComponents(
+			new ActionRowBuilder<ButtonBuilder>().addComponents(
+				new ButtonBuilder()
+					.setStyle(ButtonStyle.Primary)
+					.setLabel(await translateContext(interaction, "economy/profile:ACHIEVEMENTS"))
+					.setCustomId(`${ACHIEVEMENTS_BUTTON_ID}-${user.id}`),
+			),
 		);
 
 	interaction.editReply({
@@ -92,3 +125,114 @@ export const run = async ({ interaction }: SlashCommandProps) => {
 		},
 	});
 };
+
+// TODO: Rewrite this with /achievements
+client.on("interactionCreate", async interaction => {
+	if (!interaction.isButton()) return;
+	if (!interaction.customId.includes(ACHIEVEMENTS_BUTTON_ID)) return;
+
+	await interaction.deferUpdate();
+
+	const userId = interaction.customId.split("-")[1];
+	const user = client.users.cache.get(userId);
+	if (!user) return;
+
+	const userData = await client.getUserData(user.id);
+
+	const embed = createEmbed({
+		author: {
+			name: await translateContext(interaction, "economy/achievements:TITLE"),
+			iconURL: user.displayAvatarURL(),
+		},
+		fields: [
+			{
+				name: await translateContext(interaction, "economy/achievements:SEND_CMD"),
+				value: await translateContext(interaction, "economy/achievements:PROGRESS", {
+					now: userData.achievements.firstCommand.progress.now,
+					total: userData.achievements.firstCommand.progress.total,
+					percent: Math.round(
+						100 *
+							(userData.achievements.firstCommand.progress.now /
+								userData.achievements.firstCommand.progress.total),
+					),
+				}),
+			},
+			{
+				name: await translateContext(interaction, "economy/achievements:CLAIM_SALARY"),
+				value: await translateContext(interaction, "economy/achievements:PROGRESS", {
+					now: userData.achievements.work.progress.now,
+					total: userData.achievements.work.progress.total,
+					percent: Math.round(
+						100 *
+							(userData.achievements.work.progress.now /
+								userData.achievements.work.progress.total),
+					),
+				}),
+			},
+			{
+				name: await translateContext(interaction, "economy/achievements:MARRY"),
+				value: await translateContext(interaction, "economy/achievements:PROGRESS", {
+					now: userData.achievements.married.progress.now,
+					total: userData.achievements.married.progress.total,
+					percent: Math.round(
+						100 *
+							(userData.achievements.married.progress.now /
+								userData.achievements.married.progress.total),
+					),
+				}),
+			},
+			{
+				name: await translateContext(interaction, "economy/achievements:SLOTS"),
+				value: await translateContext(interaction, "economy/achievements:PROGRESS", {
+					now: userData.achievements.slots.progress.now,
+					total: userData.achievements.slots.progress.total,
+					percent: Math.round(
+						100 *
+							(userData.achievements.slots.progress.now /
+								userData.achievements.slots.progress.total),
+					),
+				}),
+			},
+			{
+				name: await translateContext(interaction, "economy/achievements:TIP"),
+				value: await translateContext(interaction, "economy/achievements:PROGRESS", {
+					now: userData.achievements.tip.progress.now,
+					total: userData.achievements.tip.progress.total,
+					percent: Math.round(
+						100 *
+							(userData.achievements.tip.progress.now /
+								userData.achievements.tip.progress.total),
+					),
+				}),
+			},
+			{
+				name: await translateContext(interaction, "economy/achievements:REP"),
+				value: await translateContext(interaction, "economy/achievements:PROGRESS", {
+					now: userData.achievements.rep.progress.now,
+					total: userData.achievements.rep.progress.total,
+					percent: Math.round(
+						100 *
+							(userData.achievements.rep.progress.now /
+								userData.achievements.rep.progress.total),
+					),
+				}),
+			},
+			{
+				name: await translateContext(interaction, "economy/achievements:INVITE"),
+				value: await translateContext(interaction, "economy/achievements:PROGRESS", {
+					now: userData.achievements.invite.progress.now,
+					total: userData.achievements.invite.progress.total,
+					percent: Math.round(
+						100 *
+							(userData.achievements.invite.progress.now /
+								userData.achievements.invite.progress.total),
+					),
+				}),
+			},
+		],
+	});
+
+	interaction.message.reply({
+		embeds: [embed],
+	});
+});
