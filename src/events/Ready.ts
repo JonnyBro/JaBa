7d62cbf5b2488625c05302b887d5c9ad1ad95f9c
Slincnik <pattyshow21@mail.ru>
2025-05-16 22:53:45 +0300

    feature(paths): changed to relative paths

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,10 +1,11 @@
+import { PROJECT_ROOT } from "@/constants/index.js";
 import { getNoun, getUsername } from "@/helpers/functions.js";
 import logger from "@/helpers/logger.js";
 import { CronManager } from "@/services/cron/index.js";
 import { ExtendedClient } from "@/structures/client.js";
 import loadCronTasks from "@/utils/loadCronTasks.js";
 import { ActivityType } from "discord.js";
-import { resolve } from "node:path";
+import { join } from "node:path";
 
 export const data = {
 	name: "ready",
@@ -34,7 +35,7 @@ export async function run(client: ExtendedClient) {
 	// Fetching all app emojis, because we need to use them
 	await client.application.emojis.fetch();
 
-	const tasksPath = resolve(client.configService.get("paths.tasks"));
+	const tasksPath = join(PROJECT_ROOT, "helpers", "tasks");
 	const cronTasks = await loadCronTasks(tasksPath);
 	const cronManager = new CronManager(cronTasks);
 	await cronManager.init();
