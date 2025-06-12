c374532c2c0cdd3ec856d8dc5c6b5e26300091e9
Jonny_Bro <48434875+JonnyBro@users.noreply.github.com>
2025-06-12 13:45:34 +0500

    refactor: rewrite `birthdate` to subcommands

--- src/commands/Economy/birthdate.ts
+++ src/commands/Economy/birthdate.ts
@@ -25,43 +25,60 @@ export const data: CommandData = {
 	],
 	options: [
 		{
-			name: "day",
-			...getLocalizedDesc("economy/birthdate:DAY"),
-			type: ApplicationCommandOptionType.Integer,
-		},
-		{
-			name: "month",
-			...getLocalizedDesc("economy/birthdate:MONTH"),
-			type: ApplicationCommandOptionType.Integer,
-			choices: [
-				{ name: client.i18n.translate("misc:MONTHS:JANUARY"), value: 1 },
-				{ name: client.i18n.translate("misc:MONTHS:FEBRUARY"), value: 2 },
-				{ name: client.i18n.translate("misc:MONTHS:MARCH"), value: 3 },
-				{ name: client.i18n.translate("misc:MONTHS:APRIL"), value: 4 },
-				{ name: client.i18n.translate("misc:MONTHS:MAY"), value: 5 },
-				{ name: client.i18n.translate("misc:MONTHS:JUNE"), value: 6 },
-				{ name: client.i18n.translate("misc:MONTHS:JULY"), value: 7 },
-				{ name: client.i18n.translate("misc:MONTHS:AUGUST"), value: 8 },
-				{ name: client.i18n.translate("misc:MONTHS:SEPTEMBER"), value: 9 },
-				{ name: client.i18n.translate("misc:MONTHS:OCTOBER"), value: 10 },
-				{ name: client.i18n.translate("misc:MONTHS:NOVEMBER"), value: 11 },
-				{ name: client.i18n.translate("misc:MONTHS:DECEMBER"), value: 12 },
+			name: "clear",
+			...getLocalizedDesc("economy/birthdate:CLEAR"),
+			type: ApplicationCommandOptionType.Subcommand,
+			options: [
+				{
+					name: "ephemeral",
+					type: ApplicationCommandOptionType.Boolean,
+					...getLocalizedDesc("misc:EPHEMERAL_RESPONSE"),
+				},
 			],
 		},
 		{
-			name: "year",
-			...getLocalizedDesc("economy/birthdate:YEAR"),
-			type: ApplicationCommandOptionType.Integer,
-		},
-		{
-			name: "clear",
-			type: ApplicationCommandOptionType.Boolean,
-			...getLocalizedDesc("economy/birthdate:CLEAR"),
-		},
-		{
-			name: "ephemeral",
-			type: ApplicationCommandOptionType.Boolean,
-			...getLocalizedDesc("misc:EPHEMERAL_RESPONSE"),
+			name: "set",
+			...getLocalizedDesc("economy/birthdate:SET"),
+			type: ApplicationCommandOptionType.Subcommand,
+			options: [
+				{
+					name: "day",
+					...getLocalizedDesc("economy/birthdate:DAY"),
+					type: ApplicationCommandOptionType.Integer,
+					required: true,
+				},
+				{
+					name: "month",
+					...getLocalizedDesc("economy/birthdate:MONTH"),
+					type: ApplicationCommandOptionType.Integer,
+					required: true,
+					choices: [
+						{ name: client.i18n.translate("misc:MONTHS:JANUARY"), value: 1 },
+						{ name: client.i18n.translate("misc:MONTHS:FEBRUARY"), value: 2 },
+						{ name: client.i18n.translate("misc:MONTHS:MARCH"), value: 3 },
+						{ name: client.i18n.translate("misc:MONTHS:APRIL"), value: 4 },
+						{ name: client.i18n.translate("misc:MONTHS:MAY"), value: 5 },
+						{ name: client.i18n.translate("misc:MONTHS:JUNE"), value: 6 },
+						{ name: client.i18n.translate("misc:MONTHS:JULY"), value: 7 },
+						{ name: client.i18n.translate("misc:MONTHS:AUGUST"), value: 8 },
+						{ name: client.i18n.translate("misc:MONTHS:SEPTEMBER"), value: 9 },
+						{ name: client.i18n.translate("misc:MONTHS:OCTOBER"), value: 10 },
+						{ name: client.i18n.translate("misc:MONTHS:NOVEMBER"), value: 11 },
+						{ name: client.i18n.translate("misc:MONTHS:DECEMBER"), value: 12 },
+					],
+				},
+				{
+					name: "year",
+					...getLocalizedDesc("economy/birthdate:YEAR"),
+					type: ApplicationCommandOptionType.Integer,
+					required: true,
+				},
+				{
+					name: "ephemeral",
+					type: ApplicationCommandOptionType.Boolean,
+					...getLocalizedDesc("misc:EPHEMERAL_RESPONSE"),
+				},
+			],
 		},
 	],
 };
@@ -71,40 +88,50 @@ export const run = async ({ interaction }: SlashCommandProps) => {
 		flags: interaction.options.getBoolean("ephemeral") ? MessageFlags.Ephemeral : undefined,
 	});
 
+	const subcommand = interaction.options.getSubcommand();
 	const userData = await client.getUserData(interaction.user.id);
 
-	if (interaction.options.getBoolean("clear")) {
-		userData.set("birthdate", null);
-		await userData.save();
+	switch (subcommand) {
+		case "clear":
+			userData.set("birthdate", null);
+			await userData.save();
 
-		return editReplySuccess(interaction, "economy/birthdate:SUCCESS", { date: "none" });
+			return editReplySuccess(interaction, "economy/birthdate:SUCCESS_CLEAR");
+
+		case "set": {
+			const day = interaction.options.getInteger("day", true);
+			const month = interaction.options.getInteger("month", true);
+			const year = interaction.options.getInteger("year", true);
+			const date = new Date(year, month - 1, day); // NOTE: Months are 0-based in JS
+
+			// This should be the same day in all timezones
+			date.setHours(13);
+
+			const time = date.getTime();
+
+			if (
+				!(
+					day === date.getDate() &&
+					month === date.getMonth() + 1 &&
+					year === date.getFullYear()
+				)
+			) {
+				return editReplyError(interaction, "economy/birthdate:INVALID_DATE");
+			}
+			if (date.getTime() > Date.now()) {
+				return editReplyError(interaction, "economy/birthdate:DATE_TOO_HIGH");
+			}
+			if (date.getTime() < Date.now() - 2.523e12) {
+				return editReplyError(interaction, "economy/birthdate:DATE_TOO_LOW");
+			}
+
+			userData.set("birthdate", time);
+
+			await userData.save();
+
+			return editReplySuccess(interaction, "economy/birthdate:SUCCESS", {
+				date: `<t:${Math.floor(time / 1000)}:D>`,
+			});
+		}
 	}
-
-	const day = interaction.options.getInteger("day", true),
-		month = interaction.options.getInteger("month", true),
-		year = interaction.options.getInteger("year", true),
-		date = new Date(year, month - 1, day);
-
-	// This should be the same day in all timezones
-	date.setHours(13);
-
-	const d = date.getTime();
-
-	if (!(day === date.getDate() && month - 1 === date.getMonth() && year === date.getFullYear())) {
-		return editReplyError(interaction, "economy/birthdate:INVALID_DATE");
-	}
-	if (date.getTime() > Date.now()) {
-		return editReplyError(interaction, "economy/birthdate:DATE_TOO_HIGH");
-	}
-	if (date.getTime() < Date.now() - 2.523e12) {
-		return editReplyError(interaction, "economy/birthdate:DATE_TOO_LOW");
-	}
-
-	userData.set("birthdate", d);
-
-	await userData.save();
-
-	return editReplySuccess(interaction, "economy/birthdate:SUCCESS", {
-		date: `<t:${Math.floor(d / 1000)}:D>`,
-	});
 };
