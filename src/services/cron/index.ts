29f982d422252b3c8f258b0b7b8601c98a0d4929
Jonny <jonnyar226@gmail.com>
2025-07-25 19:09:12 +0500

    refactor: rewrite cronManager to be a singleton

--- src/services/cron/index.ts
+++ src/services/cron/index.ts
@@ -3,37 +3,45 @@ import logger from "../../helpers/logger.js";
 import { CronTaskData } from "@/types.js";
 
 export class CronManager {
-	jobs = new Map<string, { job: CronJob; isRunning: boolean; isError: boolean }>();
-	tasks: CronTaskData[];
-	constructor(tasks: CronTaskData[]) {
-		this.tasks = tasks;
+	private static instance: CronManager;
+	private jobs = new Map<string, { job: CronJob; isRunning: boolean; isError: boolean }>();
+
+	private constructor() {}
+
+	public static getInstance(): CronManager {
+		if (!CronManager.instance) {
+			CronManager.instance = new CronManager();
+		}
+		return CronManager.instance;
 	}
 
-	async init() {
-		if (!this.tasks.length) {
-			logger.warn("No cron tasks to schedule.");
-			return;
-		}
+	async init(tasks: CronTaskData[]) {
+		if (!tasks.length) return logger.warn("No cron tasks to schedule.");
 
-		for (const task of this.tasks) {
-			const taskJob = this.jobs.get(task.name);
-			if (taskJob?.isRunning) {
-				logger.warn(`Cron task "${task.name}" is already running.`);
+		this.stopAll();
+
+		for (const task of tasks) {
+			if (this.jobs.has(task.name)) {
+				logger.warn(`Cron task "${task.name}" is already registered.`);
 				continue;
 			}
 
 			const job = new CronJob(
 				task.schedule,
 				async () => {
-					if (this.jobs.get(task.name)?.isError) {
-						return;
-					}
+					const jobInfo = this.jobs.get(task.name);
+					if (!jobInfo || jobInfo.isError) return;
 
 					try {
+						this.jobs.set(task.name, { ...jobInfo, isRunning: true });
+
 						await task.task();
+
+						this.jobs.set(task.name, { ...jobInfo, isRunning: false, isError: false });
 					} catch (error) {
 						logger.error(`Error executing cron task "${task.name}":`, error);
-						this.jobs.get(task.name)!.isError = true;
+
+						this.jobs.set(task.name, { ...jobInfo, isRunning: false, isError: true });
 					}
 				},
 				null,
@@ -44,19 +52,24 @@ export class CronManager {
 			job.start();
 			this.jobs.set(task.name, {
 				job,
-				isRunning: true,
+				isRunning: false,
 				isError: false,
 			});
 		}
-		logger.log(`Cron tasks scheduled: ${this.jobs.size}`);
+		logger.log(`Scheduled ${this.jobs.size} cron tasks`);
 	}
 
 	stopAll() {
 		if (!this.jobs.size) return;
 
-		for (const [_, jobInfo] of this.jobs.entries()) {
+		for (const [name, jobInfo] of this.jobs.entries()) {
 			jobInfo.job.stop();
+
+			logger.debug(`Stopped cron task "${name}"`);
 		}
-		logger.log("All cron jobs stopped.");
+
+		this.jobs.clear();
+
+		logger.log("All cron jobs stopped and cleared.");
 	}
 }
